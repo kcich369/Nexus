038f859f6dcd384dd21187fa7b5e05eb4d1bac91
Dragonet <kcich369@gmail.com>
2024-12-30 01:53:06 +0100

    changes

--- Nexus.Shared.Endpoints/Example/CreateMeeting.cs
+++ Nexus.Shared.Endpoints/Example/CreateMeeting.cs
@@ -3,7 +3,6 @@ using Nexus.Shared.Endpoints.Routes;
 
 namespace Nexus.Shared.Endpoints;
 
-public class CreateMeeting : PostEndpoint<CreateMeetingCommand, CreateMeetingCommandResult>(MeetingsRoutes.Create)
+public class CreateMeeting() : PostEndpoint<CreateMeetingCommand, CreateMeetingCommandResult>(MeetingsRoutes.Create)
 {
-  
 }
