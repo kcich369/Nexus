21be174892614b6c082b6bd82384710a5218e92c
Dragonet <kcich369@gmail.com>
2025-01-17 18:56:18 +0100

    changes

---
+++ Nexus.User.Contracts/UserData/GetUserDataQuery.cs
@@ -0,0 +1,11 @@
+﻿using Nexus.Shared.Cqrs.Interfaces;
+
+namespace Nexus.User.Contracts.UserData;
+
+public class GetUserDataQuery: IQuery<GetUserDataQueryResult>
+{
+}
+
+public class GetUserDataQueryResult: IQueryResult
+{
+}
