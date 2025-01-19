25e6b158a357dbd247bfb059076b43b9aa6c9a2d
Dragonet <kcich369@gmail.com>
2025-01-19 13:25:47 +0100

    changes

---
+++ Nexus.Shared.DbScriptsManager/ConfigurationBuilderHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using Microsoft.Extensions.Configuration;
+
+namespace Nexus.Shared.DbScriptsManager;
+
+public static class ConfigurationBuilderHelper
+{
+    private const string AppSettingsJson = "appsettings.json";
+    private const string ConnectionStrings = "ConnectionStrings";
+
+    public static IConfigurationBuilder CreateBuilder()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(AppSettingsJson, optional: false, reloadOnChange: true);
+
+        return builder;
+    }
+
+    public static IConfiguration Builds(this IConfigurationBuilder builder) =>
+        builder.Build();
+
+    public static T BindSection<T>(this IConfiguration configuration, string? sectionName = null) where T : new()
+    {
+        var instance = new T();
+        sectionName ??= GetSectionName(typeof(T).Name);
+        configuration.GetSection(sectionName).Bind(instance);
+        return instance;
+    }
+
+    private static readonly ImmutableList<string> ConfigClassNames =
+        ImmutableList.Create("Configurations", "Configuration", "Config", "Section");
+
+    private static string GetSectionName(string name)
+    {
+        if (!ConfigClassNames.Contains(name))
+            return name;
+        ConfigClassNames.ForEach(x => name = name.Replace(x, string.Empty));
+        return name;
+    }
+
+    public static string GetConnectionString(this IConfiguration configuration, string name)
+    {
+        var value = configuration.GetConnectionString(name);
+        return value;
+    }
+    public static string GetConnectionString(this IConfiguration configuration)
+    {
+        var connectionStringsSection = configuration.GetSection(ConnectionStrings);
+        var value = connectionStringsSection.GetChildren()
+            .First()
+            .Value;
+        return value!;
+    }
+}
---
+++ Nexus.Shared.DbScriptsManager/RegisterDbUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Nexus.Shared.DbScriptsManager;
+
+public static class RegisterDbUp
+{
+    public static void Register(string? connectionStringName = null)
+    {
+        var config =ConfigurationBuilderHelper.CreateBuilder().Builds();
+        var conncectionString = "";
+        if(!connectionStringName.IsNullOrEmpty())
+            conncectionString = config.GetConnectionString(connectionStringName);
+        conncectionString = config.GetConnectionString();
+
+    }
+}
